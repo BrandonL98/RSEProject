9e07ab02307ed73266a666df340106c9571e6da5
Cindy Zhou <z5209956@ad.unsw.edu.au>
2019-05-27 22:05:29 +1000

    Cindy - make working for 1 person

--- recognize_video.py
+++ recognize_video.py
@@ -2,7 +2,7 @@
 # python recognize_video.py --detector face_detection_model --embedding-model openface_nn4.small2.v1.t7 --recognizer output/recognizer.pickle --le output/le.pickle
 
 # import the necessary packages
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, abort, session
 from imutils.video import VideoStream
 from imutils.video import FPS
 import numpy as np
@@ -13,161 +13,165 @@ import time
 import cv2
 import os
 
-# Flask stuff
 app = Flask(__name__)
 
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-d", "--detector", required=True,
-	help="path to OpenCV's deep learning face detector")
-ap.add_argument("-m", "--embedding-model", required=True,
-	help="path to OpenCV's deep learning face embedding model")
-ap.add_argument("-r", "--recognizer", required=True,
-	help="path to model trained to recognize faces")
-ap.add_argument("-l", "--le", required=True,
-	help="path to label encoder")
-ap.add_argument("-c", "--confidence", type=float, default=0.5,
-	help="minimum probability to filter weak detections")
-args = vars(ap.parse_args())
+@app.route("/", methods=["GET", "POST"])
+@app.route("/home", methods=["GET", "POST"])
+def home():
+	if request.method == "POST":
+		if request.form["button"] == "start":
+			return redirect(url_for('demo'))
+	else:
+		return render_template('home.html')
 
-# load our serialized face detector from disk
-print("[INFO] loading face detector...")
-protoPath = os.path.sep.join([args["detector"], "deploy.prototxt"])
-modelPath = os.path.sep.join([args["detector"],
-	"res10_300x300_ssd_iter_140000.caffemodel"])
-detector = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
+@app.route("/demo", methods=["GET", "POST"])
+def demo():
+	# construct the argument parser and parse the arguments
 
-# load our serialized face embedding model from disk
-print("[INFO] loading face recognizer...")
-embedder = cv2.dnn.readNetFromTorch(args["embedding_model"])
+	identify_record = {}
 
-# load the actual face recognition model along with the label encoder
-recognizer = pickle.loads(open(args["recognizer"], "rb").read())
-le = pickle.loads(open(args["le"], "rb").read())
+	ap = argparse.ArgumentParser()
+	ap.add_argument("-d", "--detector", required=True,
+		help="path to OpenCV's deep learning face detector")
+	ap.add_argument("-m", "--embedding-model", required=True,
+		help="path to OpenCV's deep learning face embedding model")
+	ap.add_argument("-r", "--recognizer", required=True,
+		help="path to model trained to recognize faces")
+	ap.add_argument("-l", "--le", required=True,
+		help="path to label encoder")
+	ap.add_argument("-c", "--confidence", type=float, default=0.5,
+		help="minimum probability to filter weak detections")
+	args = vars(ap.parse_args())
 
-# initialize the video stream, then allow the camera sensor to warm up
-print("[INFO] starting video stream...")
-vs = VideoStream(src=0).start()
-time.sleep(2.0)
+	# load our serialized face detector from disk
+	print("[INFO] loading face detector...")
+	protoPath = os.path.sep.join([args["detector"], "deploy.prototxt"])
+	modelPath = os.path.sep.join([args["detector"],
+		"res10_300x300_ssd_iter_140000.caffemodel"])
+	detector = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
 
-# start the FPS throughput estimator
-fps = FPS().start()
+	# load our serialized face embedding model from disk
+	print("[INFO] loading face recognizer...")
+	embedder = cv2.dnn.readNetFromTorch(args["embedding_model"])
 
-# loop counter
-time_counter = 0
+	# load the actual face recognition model along with the label encoder
+	recognizer = pickle.loads(open(args["recognizer"], "rb").read())
+	le = pickle.loads(open(args["le"], "rb").read())
 
-# loop over frames from the video file stream
-while True:
-	# grab the frame from the threaded video stream
-	frame = vs.read()
+	# initialize the video stream, then allow the camera sensor to warm up
+	print("[INFO] starting video stream...")
+	vs = VideoStream(src=0).start()
+	time.sleep(2.0)
 
-	# resize the frame to have a width of 600 pixels (while
-	# maintaining the aspect ratio), and then grab the image
-	# dimensions
-	frame = imutils.resize(frame, width=600)
-	(h, w) = frame.shape[:2]
+	# start the FPS throughput estimator
+	fps = FPS().start()
 
-	# construct a blob from the image
-	imageBlob = cv2.dnn.blobFromImage(
-		cv2.resize(frame, (300, 300)), 1.0, (300, 300),
-		(104.0, 177.0, 123.0), swapRB=False, crop=False)
+	# loop over frames from the video file stream
+	while True:
+		# grab the frame from the threaded video stream
+		frame = vs.read()
 
-	# apply OpenCV's deep learning-based face detector to localize
-	# faces in the input image
-	detector.setInput(imageBlob)
-	detections = detector.forward()
+		# resize the frame to have a width of 600 pixels (while
+		# maintaining the aspect ratio), and then grab the image
+		# dimensions
+		frame = imutils.resize(frame, width=600)
+		(h, w) = frame.shape[:2]
 
-	# loop over the detections
-	for i in range(0, detections.shape[2]):
-		# extract the confidence (i.e., probability) associated with
-		# the prediction
-		confidence = detections[0, 0, i, 2]
+		# construct a blob from the image
+		imageBlob = cv2.dnn.blobFromImage(
+			cv2.resize(frame, (300, 300)), 1.0, (300, 300),
+			(104.0, 177.0, 123.0), swapRB=False, crop=False)
 
-		# filter out weak detections
-		if confidence > args["confidence"]:
-			# compute the (x, y)-coordinates of the bounding box for
-			# the face
-			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-			(startX, startY, endX, endY) = box.astype("int")
+		# apply OpenCV's deep learning-based face detector to localize
+		# faces in the input image
+		detector.setInput(imageBlob)
+		detections = detector.forward()
 
-			# extract the face ROI
-			face = frame[startY:endY, startX:endX]
-			(fH, fW) = face.shape[:2]
+		# loop over the detections
+		for i in range(0, detections.shape[2]):
+			# extract the confidence (i.e., probability) associated with
+			# the prediction
+			confidence = detections[0, 0, i, 2]
 
-			# ensure the face width and height are sufficiently large
-			if fW < 20 or fH < 20:
-				continue
+			# filter out weak detections
+			if confidence > args["confidence"]:
+				# compute the (x, y)-coordinates of the bounding box for
+				# the face
+				box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+				(startX, startY, endX, endY) = box.astype("int")
 
-			# construct a blob for the face ROI, then pass the blob
-			# through our face embedding model to obtain the 128-d
-			# quantification of the face
-			faceBlob = cv2.dnn.blobFromImage(face, 1.0 / 255,
-				(96, 96), (0, 0, 0), swapRB=True, crop=False)
-			embedder.setInput(faceBlob)
-			vec = embedder.forward()
+				# extract the face ROI
+				face = frame[startY:endY, startX:endX]
+				(fH, fW) = face.shape[:2]
 
-			# perform classification to recognize the face
-			preds = recognizer.predict_proba(vec)[0]
-			j = np.argmax(preds)
-			proba = preds[j]
-			name = le.classes_[j]
+				# ensure the face width and height are sufficiently large
+				if fW < 20 or fH < 20:
+					continue
 
-			# draw the bounding box of the face along with the
-			# associated probability
-			text = "{}: {:.2f}%".format(name, proba * 100)
-			y = startY - 10 if startY - 10 > 10 else startY + 10
-			cv2.rectangle(frame, (startX, startY), (endX, endY),
-				(0, 0, 255), 2)
-			cv2.putText(frame, text, (startX, y),
-				cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+				# construct a blob for the face ROI, then pass the blob
+				# through our face embedding model to obtain the 128-d
+				# quantification of the face
+				faceBlob = cv2.dnn.blobFromImage(face, 1.0 / 255,
+					(96, 96), (0, 0, 0), swapRB=True, crop=False)
+				embedder.setInput(faceBlob)
+				vec = embedder.forward()
 
-	# update the FPS counter
-	fps.update()
+				# perform classification to recognize the face
+				preds = recognizer.predict_proba(vec)[0]
+				j = np.argmax(preds)
+				proba = preds[j]
+				name = le.classes_[j]
 
-	# increment loop counter
-	time_counter = time_counter + 1
+				name_str = str(name)
+				if name_str in identify_record:
+					identify_record[name_str] = identify_record[name_str] + proba
+				else:
+					identify_record[name_str] = proba
 
-	# show the output frame
-	cv2.imshow("Frame", frame)
-	key = cv2.waitKey(1) & 0xFF
+				# draw the bounding box of the face along with the
+				# associated probability
+				text = "{}: {:.2f}%".format(name, proba * 100)
+				y = startY - 10 if startY - 10 > 10 else startY + 10
+				cv2.rectangle(frame, (startX, startY), (endX, endY),
+					(0, 0, 255), 2)
+				cv2.putText(frame, text, (startX, y),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
 
-	# endpoint of flask
-	if proba*100 >= 70 and time_counter >= 10:
-		names = str(name)
+		# update the FPS counter
+		fps.update()
 
-		# send name through endpoint
-		@app.route("/name")
-		def print_name():
-			return_string = ""
-			return_string = return_string + names
-			return return_string
-		
-		# checks if name is on the homeowner's list
-		@app.route("/")
-		def auth():
-			f = open("homeowners.txt", "r")
-			for line in f:
-				line = line.rstrip()
-				if line == names:
-					return str(1)
-				elif line == '':
-					return str(0)
-		break
+		# show the output frame
+		cv2.imshow("Frame", frame)
+		key = cv2.waitKey(1) & 0xFF
 
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
+		# if the `q` key was pressed, break from the loop
+		if key == ord("q"):
+			break
 
-# flask end
-if __name__ == "__main__":
-    app.run()
+	# stop the timer and display FPS information
+	fps.stop()
+	print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+	print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
 
-# stop the timer and display FPS information
-fps.stop()
-print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
-print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+	# do a bit of cleanup
+	cv2.destroyAllWindows()
+	vs.stop()
 
-# do a bit of cleanup
-cv2.destroyAllWindows()
-vs.stop()
+	# check man at door and if he/she is home owner
+	door = "unknown"
+	biggest_val = -1
+	for key,val in identify_record.items():
+		if val > biggest_val:
+			door = key
+			biggest_val = val
 
+	f = open("homeowners.txt", "r")
+	for line in f:
+		line = line.rstrip()
+		if line == door:
+			return render_template('demo.html', name=door, homeowner="True")
+		elif line == '':
+			return render_template('demo.html', name=door)
+
+if __name__ == '__main__':
+    app.run(debug=True, port = 8000)
