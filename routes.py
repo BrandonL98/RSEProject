7e28766cc69caea6ca56ef626aab01e5f77e70d9
Cindy Zhou <z5209956@ad.unsw.edu.au>
2019-05-28 17:10:57 +1000

    commane line optimisation

--- routes.py
+++ routes.py
@@ -13,28 +13,19 @@ import os
 
 app = Flask(__name__)
 
-@app.route("/camera", methods=["POST","GET"])
+@app.route("/camera")
 def camera():
 
     ap = argparse.ArgumentParser()
 
-    ap.add_argument("-d", "--detector", required=True,
-		help="path to OpenCV's deep learning face detector")
-    ap.add_argument("-m", "--embedding-model", required=True,
-		help="path to OpenCV's deep learning face embedding model")
-    ap.add_argument("-r", "--recognizer", required=True,
-		help="path to model trained to recognize faces")
-    ap.add_argument("-l", "--le", required=True,
-		help="path to label encoder")
-    ap.add_argument("-c", "--confidence", type=float, default=0.5,
-		help="minimum probability to filter weak detections")
     ap.add_argument("-rl", "--relearn", required=True,
 		help="if there is photo to be relearned")
     args = vars(ap.parse_args())
 
     need_to_learn = args["relearn"]
 
-    identified_record = recognize_video.process_video(need_to_learn,args["detector"],args["embedding_model"],args["recognizer"],args["le"],args["confidence"])
+    identified_record = recognize_video.process_video(need_to_learn,"face_detection_model",
+        "openface_nn4.small2.v1.t7","output/recognizer.pickle","output/le.pickle",0.5)
 
     # check man at door and if he/she is home owner
     door = "unknown"
@@ -59,7 +50,7 @@ def lock():
 	state = lock_module.check_lock_status()
 	return state
 
-@app.route('/update_lock/<state>', methods=["PUT"])
+@app.route('/update_lock/<state>', methods=["GET","PUT"])
 def update_lock(state):
     if (state == "open"):
         lock_module.open_lock()
