365d03e0619c3cad418eaf0e4203034b9871001a
Cindy Zhou <z5209956@ad.unsw.edu.au>
2019-05-29 15:52:13 +1000

    Cindy - solved merge conflict, now opencv working with a return file .json

--- routes.py
+++ routes.py
@@ -33,40 +33,27 @@ def camera():
         identified_record = recognize_video.process_video(need_to_learn,"face_detection_model",
             "openface_nn4.small2.v1.t7","output/recognizer.pickle","output/le.pickle",0.5)
 
-        # check man at door and if he/she is home owner
-        door = "unknown"
-        biggest_val = -1
-        for key,val in identified_record.items():
-            if val > biggest_val:
-                door = key
-                biggest_val = val
-
-        f = open("homeowners.txt", "r")
-        for line in f:
-            line = line.rstrip()
-            if line == door:
-                lock_module.open_lock()
-                return render_template('demo.html', name=door, homeowner="True")
-            elif line == '':
-                return render_template('demo.html', name=door)
-
-        return render_template('demo.html', name=door)
-
-    ap = argparse.ArgumentParser()
-
-    ap.add_argument("-rl", "--relearn", required=True,
-		help="if there is photo to be relearned")
-    args = vars(ap.parse_args())
-
-    need_to_learn = args["relearn"]
-
-    identified_record = recognize_video.process_video(need_to_learn,"face_detection_model",
-        "openface_nn4.small2.v1.t7","output/recognizer.pickle","output/le.pickle",0.5)
-
     door = json_operations.readFromJSONFile('whos_at_door')
 
     # check man at door and if he/she is home owner
     return render_template('demo.html', name=list(door.keys())[0])
+
+@app.route("/user", methods=["GET", "POST"])
+def user():
+    if request.method == "POST":
+        if request.form["button"] == "lock":
+            lock_module.lock_lock()
+            return redirect(url_for('user'))
+        elif request.form["button"] == "unlock":
+            lock_module.open_lock()
+            return redirect(url_for('user'))
+        elif request.form["button"] == 'camera':
+            return redirect(url_for('camera'))
+        elif request.form["button"] == 'back':
+            return redirect(url_for('home'))
+    else: 
+	    state = lock_module.check_lock_status()
+	    return render_template('lock.html', lock_status = state)
 
 @app.route("/lock")
 def lock():
